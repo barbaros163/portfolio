30faf7b93085912b357a64d54171c95a7a02f0a9
Yasin Yavuz <barbaros1675@gmail.com>
2023-01-15 15:31:24 +0100

    navbar and footer added

---
+++ src/components/Footer.js
@@ -0,0 +1,20 @@
+import React from "react";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import Email from "@material-ui/icons/Email";
+import Github from "@material-ui/icons/GitHub";
+import "../styles/Footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="socialMedia">
+        <LinkedInIcon />
+        <Email />
+        <Github />
+      </div>
+      <p>&copy; 2023 digitaldesigns.at </p>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/pages/Home.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div className="home">
+      <div>
+        <div className="about">
+          <h2>Hi, My Name is Yasin</h2>
+          <div className="prompt">
+            A passionate Frontend developer from Anatolien
+          </div>
+        </div>
+        <div className="skills"></div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;

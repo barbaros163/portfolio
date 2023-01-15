13f3dcc837d3af59bcc9be75bc4b24c0e6ef2ace
Yasin Yavuz <barbaros1675@gmail.com>
2023-01-15 17:11:51 +0100

    experience must be changed

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,8 @@
 import React from "react";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import Email from "@material-ui/icons/Email";
+import Github from "@material-ui/icons/GitHub";
+import "../styles/Home.css";
 
 function Home() {
   return (
@@ -7,10 +11,32 @@ function Home() {
         <div className="about">
           <h2>Hi, My Name is Yasin</h2>
           <div className="prompt">
-            A passionate Frontend developer from Anatolien
+            <p> A passionate Frontend developer from Anatolien</p>
+            <LinkedInIcon />
+            <Email />
+            <Github />
           </div>
         </div>
-        <div className="skills"></div>
+        <div className="skills">
+          <h1>Skills</h1>
+          <ol className="list">
+            <li className="item">
+              <h2>Front-End</h2>
+              <span>
+                Html, Css, Javascript, Git, Agile, Jira, Trello, Bootstrap,
+                Sass, React, Kanban, Tailwindcss, Materialize, jQuery, Wordpress
+              </span>
+            </li>
+            <li className="item">
+              <h2>Back-End</h2>
+              <span>NodeJS, Firebase</span>
+            </li>
+            <li className="item">
+              <h2>Languages</h2>
+              <span>JavaScript, TypeScript</span>
+            </li>
+          </ol>
+        </div>
       </div>
     </div>
   );

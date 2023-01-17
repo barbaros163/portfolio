83526f40b5c111200bc04b534e2ba177789841f8
Yasin Yavuz <barbaros1675@gmail.com>
2023-01-17 22:30:09 +0100

    map added

---
+++ src/helpers/ProjectList.js
@@ -0,0 +1,33 @@
+import Proj1 from "../assets/1.jpg";
+import Proj2 from "../assets/2.jpg";
+import Proj3 from "../assets/3.jpg";
+import Proj4 from "../assets/4.jpg";
+import Proj5 from "../assets/5.jpg";
+import Proj6 from "../assets/6.jpg";
+
+export const projectList = [
+  {
+    name: "Dijkstra Algorithm Visualizer",
+    image: Proj1,
+  },
+  {
+    name: "Ecommerce Website",
+    image: Proj2,
+  },
+  {
+    name: "Spotify Clone",
+    image: Proj3,
+  },
+  {
+    name: "Social Media Website",
+    image: Proj4,
+  },
+  {
+    name: "Dashboard Visualizer",
+    image: Proj5,
+  },
+  {
+    name: "Mobile Game",
+    image: Proj6,
+  },
+];
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,14 +1,16 @@
 import React from "react";
 import ProjectItem from "../components/ProjectItem";
-import Proj1 from "../assets/1.jpg";
+import { ProjectList } from "../helpers/ProjectList";
+import "../styles/Project.css";
 
 function Projects() {
   return (
     <div className="projects">
       <h1>My Personal Projects</h1>
       <div className="projectList">
-        <ProjectItem name="Social Media Webseite" image={Proj1} />
-        <ProjectItem />
+        {ProjectList.map((project) => {
+          return <ProjectItem name={project.name} image={project.image} />;
+        })}
       </div>
     </div>
   );

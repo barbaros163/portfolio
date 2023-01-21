5c6e3a5a86b9d99867dfa94daeb59fcffdfffa95
Yasin Yavuz <barbaros1675@gmail.com>
2023-01-21 14:42:46 +0100

    some pictures added

--- src/helpers/ProjectList.js
+++ src/helpers/ProjectList.js
@@ -1,39 +1,40 @@
-import Proj1 from "../assets/1.jpg";
-import Proj2 from "../assets/2.jpg";
-import Proj3 from "../assets/3.jpg";
-import Proj4 from "../assets/4.jpg";
-import Proj5 from "../assets/5.jpg";
-import Proj6 from "../assets/6.jpg";
+import Proj1 from "../assets/survey.png";
+import Proj2 from "../assets/voltran.png";
+import Proj3 from "../assets/google.png";
+import Proj4 from "../assets/sass.png";
+import Proj5 from "../assets/bootstrap1.png";
+import Proj6 from "../assets/bootstrap2.gif";
+import Proj7 from "../assets/language-cards.gif";
 
 export const ProjectList = [
   {
     name: "Survey Form",
-    image: Proj4,
+    image: Proj1,
     skills: "HTML, CSS",
     url: "https://html-netflix.netlify.app/",
   },
   {
     name: "Voltran Page",
-    image: Proj5,
+    image: Proj2,
     skills: "HTML, CSS",
     url: "https://html-voltran.netlify.app/",
   },
   {
     name: "Google Landing Page",
-    image: Proj6,
+    image: Proj3,
     skills: "HTML, CSS",
     url: "https://html-google-landing.netlify.app/",
   },
 
   {
     name: "Sass Webseite",
-    image: Proj1,
+    image: Proj4,
     skills: "HTML, CSS, SASS ",
     url: "https://barbaros163.github.io/sass1_website/",
   },
   {
     name: "Bootstrap Website",
-    image: Proj2,
+    image: Proj5,
     skills: "HTML, CSS, BOOTSTRAP ",
     url: "https://bootstrap-page-1.netlify.app/",
   },
@@ -46,8 +47,8 @@ export const ProjectList = [
   },
 
   {
-    name: "React Webseite",
-    image: Proj3,
+    name: "React Language Cards",
+    image: Proj7,
     skills: "HTML, CSS, JS, ReactJS",
     url: "https://barbaros163.github.io/02_Language_Cards_React/",
   },

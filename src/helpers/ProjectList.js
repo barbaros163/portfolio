45c9e8ff0f1f725f862cb7d67651722aa6233f0d
Yasin Yavuz <barbaros1675@gmail.com>
2023-01-22 18:22:53 +0100

    gifs added

--- src/helpers/ProjectList.js
+++ src/helpers/ProjectList.js
@@ -1,27 +1,29 @@
-import Proj1 from "../assets/survey.png";
-import Proj2 from "../assets/voltran.png";
+import Proj1 from "../assets/voltran.gif";
+import Proj2 from "../assets/survey.gif";
 import Proj3 from "../assets/google.png";
-import Proj4 from "../assets/sass.png";
-import Proj5 from "../assets/bootstrap1.png";
-import Proj6 from "../assets/bootstrap2.gif";
-import Proj7 from "../assets/tour-places.gif";
-import Proj8 from "../assets/language-cards.gif";
-import Proj9 from "../assets/recipe.gif";
-import Proj10 from "../assets/weather_app.gif";
+import Proj4 from "../assets/parallax.gif";
+import Proj5 from "../assets/sass.gif";
+import Proj6 from "../assets/bootstrap1.gif";
+import Proj7 from "../assets/bootstrap2.gif";
+import Proj8 from "../assets/tour-places.gif";
+import Proj9 from "../assets/language-cards.gif";
+import Proj10 from "../assets/recipe.gif";
+import Proj11 from "../assets/weather_app.gif";
 
 export const ProjectList = [
   {
-    name: "Survey Form",
-    image: Proj1,
-    skills: "HTML, CSS",
-    url: "https://html-netflix.netlify.app/",
-  },
-  {
     name: "Voltran Page",
-    image: Proj2,
+    image: Proj1,
     skills: "HTML, CSS",
     url: "https://html-voltran.netlify.app/",
   },
+  {
+    name: "Survey Form",
+    image: Proj2,
+    skills: "HTML, CSS",
+    url: "https://html-netflix.netlify.app/",
+  },
+
   {
     name: "Google Landing Page",
     image: Proj3,
@@ -30,47 +32,54 @@ export const ProjectList = [
   },
 
   {
-    name: "Sass Webseite",
+    name: "Parallax Webseite",
     image: Proj4,
+    skills: "HTML, CSS",
+    url: "https://barbaros163.github.io/20.08.2022_parallax_website/",
+  },
+
+  {
+    name: "Sass Webseite",
+    image: Proj5,
     skills: "HTML, CSS, SASS ",
     url: "https://barbaros163.github.io/sass1_website/",
   },
   {
     name: "Bootstrap Website",
-    image: Proj5,
+    image: Proj6,
     skills: "HTML, CSS, BOOTSTRAP ",
     url: "https://bootstrap-page-1.netlify.app/",
   },
 
   {
     name: "Bootstrap Webseite 2",
-    image: Proj6,
+    image: Proj7,
     skills: "HTML, CSS, BOOTSTRAP",
     url: "https://bootstrap-page-2.netlify.app/",
   },
 
   {
     name: "React Tour Places",
-    image: Proj7,
+    image: Proj8,
     skills: "HTML, CSS, JavaScript, ReactJS",
     url: "https://barbaros163.github.io/01_Tour_Places_React/",
   },
   {
     name: "React Language Cards",
-    image: Proj8,
+    image: Proj9,
     skills: "HTML, CSS, JavaScript, ReactJS",
     url: "https://barbaros163.github.io/02_Language_Cards_React/",
   },
   {
     name: "React Recipe App",
-    image: Proj9,
+    image: Proj10,
     skills: "HTML, CSS, JavaScript, ReactJS",
     url: "https://barbaros163.github.io/05_Recipe_App_Solution_react/",
   },
 
   {
     name: "Weather App",
-    image: Proj10,
+    image: Proj11,
     skills: "HTML, CSS, JavaScript",
     url: "https://barbaros163.github.io/jQuery_js/weather.html",
   },
